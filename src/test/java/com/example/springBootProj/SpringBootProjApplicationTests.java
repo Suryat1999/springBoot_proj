640933707e78d0b7122820ce03bdbc5f7aacce32
Surya T <suryathangam765@gmail.com>
2025-11-28 18:06:03 +0800

    files are moved to git repositary

---
+++ src/test/java/com/example/springBootProj/SpringBootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
